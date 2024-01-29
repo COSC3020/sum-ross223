fa5ae5c1d45d64d0c998126c71290a717bbe5b31
ross223 <144744308+ross223@users.noreply.github.com>
2024-01-29 13:48:15 -0700

    Update code.js

--- code.js
+++ code.js
@@ -5,3 +5,21 @@ function sum(a) {
     }
     return sum;
 }
+
+public class Main
+{
+    static int sum(int[] a) {
+	    int total = 0;
+        var sum = a;
+        for(var i = 0; i < a.length; i++) {
+            total += a[i];
+        }
+        return total;
+    }
+    
+    public static void main(String[] args){
+	    int[] test = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	    int thing = sum(test);
+	    System.out.println(thing);
+    }
+}
